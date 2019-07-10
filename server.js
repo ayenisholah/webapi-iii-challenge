7f0f2d55f37613a3ebc5aeb1e4dcff41c626da7e
ayenisholah <Shola@Andelas-MBP.localdomain>
2019-07-10 13:04:43 +0100

    logger middleware

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = 'express';
+const express = require('express');
+// const postsRouter = require('./posts/postRouter.js')
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.url} ${req.get('Origin')} on [${new Date().toISOString()}]`);
+};
 
 const server = express();
+server.use(express.json());
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
