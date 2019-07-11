7a9826de361ecddc8aa82d507f26c3e6bbe586ea
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-11 18:36:01 +0100

    WIP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const server = require('./server');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3333;
 
 server.listen(port, () => {
   console.log('\n *** Magic Happening on http://localhost:3333 ***\n')
