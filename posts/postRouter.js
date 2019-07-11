c3ac1c1ba3a03ce1d2e2cd27789fb237c0b423ea
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-11 14:44:51 +0100

    endpoint to get post by id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -17,7 +17,18 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', (req, res) => {
   try {
-    const post = await Posts.getById(re)
+    const post = await Posts.getById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving post'
+    });
   }
 });
 
