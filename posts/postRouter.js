9bfef2121d1595d724882876dd944f983307fd84
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-11 14:40:53 +0100

    Endpoint for get all posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,24 @@
 const express = require('express');
-
+const Posts = require('./postDb.js');
 const router = express.Router();
 
-router.get('/', (req, res) => {
 
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Posts.get();
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving posts'
+    });
+  }
 });
 
 router.get('/:id', (req, res) => {
-
+  try {
+    const post = await Posts.getById(re)
+  }
 });
 
 router.delete('/:id', (req, res) => {
